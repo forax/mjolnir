c1ee6e5d9945d2dd5f652a894e767219906e1aa9
forax <forax@univ-mlv.fr>
2017-09-11 15:03:59 +0200

    update travisCI to use pro 0.9b181b

--- get_pro.sh
+++ get_pro.sh
@@ -1,5 +1,5 @@
 # get pro to build the project
-wget --no-check-certificate https://github.com/forax/pro/releases/download/v0.9.181/pro-9-b181b-linux.tar.gz
+wget --no-check-certificate https://github.com/forax/pro/releases/download/v0.9.181b/pro-9-b181b-linux.tar.gz
 tar -xzf pro-9-b181b-linux.tar.gz
 
 # then run
